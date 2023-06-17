5303eff0ee6c72021d3c3ea1a359dbae1db341fe
jaya shinde <jayashinde306@gmail.com>
2023-06-17 19:28:49 +0530

    hhh

--- CustomerManagementSystem/src/main/java/com/csi/controller/CustomerController.java
+++ CustomerManagementSystem/src/main/java/com/csi/controller/CustomerController.java
@@ -48,7 +48,7 @@ public class CustomerController extends HttpServlet {
 		 
 		 RequestDispatcher requestDispatcher=request.getRequestDispatcher("showCustData.jsp");
 		 requestDispatcher.forward(request, response);	
-system	 
+	 
 	
 	
 	}
